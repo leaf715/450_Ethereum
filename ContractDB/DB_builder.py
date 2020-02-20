9ea1b1d12ad9e60a4a116535a693c6c3bd77e832
leaf715 <shuhan2020@u.northwestern.edu>
2020-02-20 12:34:42 -0600

    DB_builder class

--- ContractDB/DB_builder.py
+++ ContractDB/DB_builder.py
@@ -3,83 +3,95 @@ import os
 import glob
 import shutil
 
-log_dir = "State_DB/"
-contracts = []
+class DB_builder:
+    def __init__(self, log_dir, level_size):
+        if not os.path.isdir(log_dir):
+            os.mkdir(log_dir)
+        self.log_dir = log_dir
+        self.level_size = level_size
 
-def get_traces(dir):
-    files = sorted([f for f in os.listdir(dir)])
-    return files
+    def get_trace(self, dir, i):
+        return dir+i+".json"
 
-def get_folder(bytestr):
-    folder = log_dir+bytestr
-    if not os.path.isdir(folder):
-        os.mkdir(folder)
-    return folder
+    def num_str8(self, i):
+        num_str = str(i)
+        return "0"*(8-len(num_str))+num_str
 
-def build_DB(restart, trace_dir):
-    if restart:
-        files = glob.glob(log_dir+'*')
-        for f in files:
-            shutil.rmtree(f)
-        f_read = open("files_read.log", "w+")
-    else:
-        f_read = open("files_read.log", "r+")
+    def get_folder(self, bytestr):
+        folder = self.log_dir+bytestr[:2+self.level_size]
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+        folder = self.log_dir+bytestr[:2+self.level_size]+"/"+bytestr[:2+2*self.level_size]
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+        folder = self.log_dir+bytestr[:2+self.level_size]+"/"+bytestr[:2+2*self.level_size]+"/"+bytestr[:2+3*self.level_size]
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+        folder = self.log_dir+bytestr[:2+self.level_size]+"/"+bytestr[:2+2*self.level_size]+"/"+bytestr[:2+3*self.level_size]+"/"+bytestr
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+        return folder
 
-    f_dir = open("dir.log", "a+")
+    def build_DB(self, trace_dir, start, end, restart = None):
 
-    contracts = f_dir.readlines()
-    # f_meta = open(log_dir+"metadata.log", "a")
-    # f_call = open(log_dir+"call.log", "a")
-    # f_sw = open(log_dir+"storage-write.log", "a")
+        if restart:
+            shutil.rmtree(self.log_dir)
+            os.mkdir(self.log_dir)
+            f_read = open(self.log_dir+"last_read.log", "w+")
+        else:
+            f_read = open(self.log_dir+"last_read.log", "r+")
 
-    traces = get_traces(trace_dir)
-    print(traces)
-    if not restart:
-        f_read.seek(0)
-        last = f_read.readline()
-        if last:
-            i = 0
-            while i < len(traces) and traces[i][:-5] != last:
-                i+=1
-            traces = traces[i:]
-    print(traces)
-    while traces:
-        trace = traces.pop(0)
-        with open(trace_dir+trace, "r") as f:
-            data = json.load(f)
-        for tx in data:
-            block_number = str(tx["block_number"])
-            trxn_id = str(tx["tx_index"])
-            for call in tx["call_level_traces"]:
-                call_id = str(call["call_id"])
-                if call["call_type"] == "CREATE" or call["call_type"] == "CREATE2" and call["success"]:
-                    bytestr = call["outputs"][-1]
-                    contract_folder = get_folder(bytstr.split("data=0x", 1)[1])
-                    f_meta = open(contract_folder+"/metadata.log", "a")
-                    f_meta.write(bytstr + " " + block_number + " " + trxn_id + " " + call_id + "\n")
-                    f_meta.close()
-                    f_dir.write(bytestr+"\n")
-                    contracts.append(bytestr)
-                output_opcodes = [code.split(" ", 1)[0] for code in call["outputs"]]
-                if "SELFDESTRUCT" in output_opcodes and call["success"]:
-                    contract_folder = get_folder(call["to"])
-                    f_meta = open(contract_folder+"/metadata.log", "a")
-                    f_meta.write(block_number + " " + trxn_id + " " + call_id + "\n")
-                    f_meta.close()
-                if call["to"] in contracts:
-                    contract_folder = get_folder(call["to"])
-                    f_call = open(contract_folder+"/call.log", "a")
-                    f_call.write(block_number + " " + trxn_id + " " + call_id + "\n")
-                    f_call.close()
-            for bytestr in tx["storage_written"]:
-                contract_folder = get_folder(bytestr)
-                f_sw = open(contract_folder+"/storage-write.log", "a")
-                for slot in tx["storage_written"][bytestr]:
-                    f_sw.write(slot + " " + block_number + " " + trxn_id + "\n")
-                f_sw.close()
-        f_read.seek(0)
-        f_read.write(trace[:-5]+"\n")
+        if restart:
+            trace = start
+        else:
+            f_read.seek(0)
+            trace = int(f_read.readline())+1
+
+
+        while trace <= end:
+            print(trace)
+            path = self.get_trace(trace_dir, self.num_str8(trace))
+            if os.path.exists(path):
+                with open(self.get_trace(trace_dir, self.num_str8(trace)), "r") as f:
+                    data = json.load(f)
+                for tx in data:
+                    block_number = str(tx["block_number"])
+                    trxn_id = str(tx["tx_index"])
+                    for call in tx["call_level_traces"]:
+                        call_id = str(call["call_id"])
+                        if call["call_type"] == "CREATE" or call["call_type"] == "CREATE2" and call["success"]:
+                            bytestr = call["outputs"][-1]
+                            contract_folder = self.get_folder(bytstr.split("data=0x", 1)[1])
+                            f_meta = open(contract_folder+"/metadata.log", "a")
+                            f_meta.write(bytstr + " " + block_number + " " + trxn_id + " " + call_id + "\n")
+                            f_meta.close()
+                        output_opcodes = [code.split(" ", 1)[0] for code in call["outputs"]]
+                        if "SELFDESTRUCT" in output_opcodes and call["success"]:
+                            contract_folder = self.get_folder(call["to"])
+                            f_meta = open(contract_folder+"/metadata.log", "a")
+                            f_meta.write(block_number + " " + trxn_id + " " + call_id + "\n")
+                            f_meta.close()
+                        if call["call_type"] in ["CALL", "STATICCALL"]: #Deal with other call types
+                            contract_folder = self.get_folder(call["to"])
+                            f_call = open(contract_folder+"/call.log", "a")
+                            f_call.write(block_number + " " + trxn_id + " " + call_id + "\n")
+                            f_call.close()
+                        if call["call_type"] in ["DELEGATECALL", "CALLCODE"]: #Deal with other call types
+                            contract_folder = self.get_folder(call["from"])
+                            f_call = open(contract_folder+"/call.log", "a")
+                            f_call.write(block_number + " " + trxn_id + " " + call_id + "\n")
+                            f_call.close()
+                    for bytestr in tx["storage_written"]:
+                        contract_folder = self.get_folder(bytestr)
+                        f_sw = open(contract_folder+"/storage-write.log", "a")
+                        for slot in tx["storage_written"][bytestr]:
+                            f_sw.write(slot + " " + block_number + " " + trxn_id + "\n")
+                        f_sw.close()
+            f_read.seek(0)
+            f_read.write(self.num_str8(trace)+"\n")
+            trace+=1
 
 
 if __name__ == "__main__":
-    build_DB(True, "traces/")
+    DB = DB_builder("State_DB/", 2)
+    DB.build_DB("traces/", 12345670, 12345680, True)
