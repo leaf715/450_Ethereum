0ec262a6217c51a268ed5e8e243cc5f7264d80df
leaf715 <shuhan2020@u.northwestern.edu>
2020-03-17 19:26:01 -0500

    evaluation

--- Final Deliverables/Transformer.py
+++ Final Deliverables/Transformer.py
@@ -38,12 +38,12 @@ class Transformer:
         num_str = str(i)
         return "0"*(8-len(num_str))+num_str
 
+    # parces trace [trace] in directory [trace_dir]
     def __init__(self, trace_dir, trace):
         self.DFG = parse_file(self.get_trace(trace_dir, self.num_str8(trace)))
-        # _pprint(self.DFG)
 
+    # transform call [call_id] in trxn [tx_id] from white box to grey box
     def greybox_call(self, tx_id, call_id):
-        print("greyboxing call")
         call_trace_dict = {}
         taint_dict = {}
         outputset = set()
@@ -63,13 +63,8 @@ class Transformer:
         greytaints = set()
         greydependencies = {}
         for taint in call.taints:
-            # if not (taint.Dsource_ids.isdisjoint(blockinfo) or taint.Isource_ids.isdisjoint(blockinfo)):
-            #     lhset.add(taint)
-            #     greytaints.add(taint)
             for id in taint.Dsource_ids:
                 if id in blockinfo:
-                    print("left blockinfo")
-                    print(taint.name)
                     if id in greydependencies:
                         greydependencies[taint.serial_id].add(GreyDependency("D", id))
                     else:
@@ -78,39 +73,24 @@ class Transformer:
                     greytaints.add(taint)
                 if id in call_trace_dict:
                     if call_trace_dict[id] != call.call_id:
-                        print("left outside source " + str(id))
-                        print(taint.name)
                         lhset.add(taint)
                         greytaints.add(taint)
                 elif id >= 0:
-                    print("left outside source - other trxn" + str(id))
-                    print(taint.name)
                     lhset.add(taint)
                     greytaints.add(taint)
             for id in taint.Ddestination_ids:
                 if id in call_trace_dict:
                     if call_trace_dict[id] != call.call_id:
-                        print("right outside dest " + str(id))
-                        print(taint.name)
                         rhset.add(taint)
                         greytaints.add(taint)
             if taint.serial_id in outputset:
-                for id in taint.Dsource_ids.union(taint.Isource_ids): #not sure if source or destination for "depending"
-                    print("right source of output")
-                    print(taint_dict[id].name)
+                for id in taint.Dsource_ids.union(taint.Isource_ids):
                     rhset.add(taint_dict[id])
                     greytaints.add(taint_dict[id])
-        print("taints tracked")
-        for taint in greytaints:
-            print(taint.name)
         for ltaint in lhset:
-            # lhs extensions
             for source_id in ltaint.Dsource_ids:
                 if source_id in call_trace_dict:
                     if call_trace_dict[source_id] == call.call_id and taint_dict[source_id] not in lhset:
-                        print("extension lhs")
-                        print(taint_dict[source_id].name)
-                        print("Direct Edge " + taint_dict[source_id].name + " to " + ltaint.name)
                         greytaints.add(taint_dict[source_id])
                         if ltaint.serial_id in greydependencies:
                             greydependencies[ltaint.serial_id].add(GreyDependency("D", source_id))
@@ -119,116 +99,84 @@ class Transformer:
             for source_id in ltaint.Isource_ids:
                 if source_id in call_trace_dict:
                     if call_trace_dict[source_id] == call.call_id:
-                        print("extension lhs")
-                        print(taint_dict[source_id].name)
-                        print("Indirect Edge " + taint_dict[source_id].name + " to " + ltaint.name)
                         greytaints.add(taint_dict[source_id])
                         if ltaint.serial_id in greydependencies:
                             greydependencies[ltaint.serial_id].add(GreyDependency("I", source_id))
                         else:
                             greydependencies[ltaint.serial_id] = set([GreyDependency("I", source_id)])
             for rtaint in rhset:
-                # edges from lhs to rhs
                 if ltaint.serial_id == rtaint.serial_id:
-                    print("Identical Edge " + ltaint.name + " to " + rtaint.name)
                     if rtaint.serial_id in greydependencies:
                         greydependencies[rtaint.serial_id].add(GreyDependency("Identical", ltaint.serial_id))
                     else:
                         greydependencies[rtaint.serial_id] = set([GreyDependency("Identical", ltaint.serial_id)])
                 elif ltaint.serial_id in rtaint.Dsource_ids and ltaint.serial_id not in rtaint.Isource_ids.union(rtaint.Csource_ids):
-                    print("Direct Edge " + ltaint.name + " to " + rtaint.name)
                     if rtaint.serial_id in greydependencies:
                         greydependencies[rtaint.serial_id].add(GreyDependency("D", ltaint.serial_id))
                     else:
                         greydependencies[rtaint.serial_id] = set([GreyDependency("D", ltaint.serial_id)])
                 elif ltaint.serial_id in rtaint.Dsource_ids.union(rtaint.Isource_ids) and ltaint.serial_id not in rtaint.Csource_ids:
-                    print("Indirect Edge " + ltaint.name + " to " + rtaint.name)
                     if rtaint.serial_id in greydependencies:
                         greydependencies[rtaint.serial_id].add(GreyDependency("I", ltaint.serial_id))
                     else:
                         greydependencies[rtaint.serial_id] = set([GreyDependency("I", ltaint.serial_id)])
                 elif ltaint.serial_id in rtaint.Dsource_ids.union(rtaint.Isource_ids, rtaint.Csource_ids):
-                    print("Control Flow Edge " + ltaint.name + " to " + rtaint.name)
                     if rtaint.serial_id in greydependencies:
                         greydependencies[rtaint.serial_id].add(GreyDependency("C", ltaint.serial_id))
                     else:
                         greydependencies[rtaint.serial_id] = set([GreyDependency("C", ltaint.serial_id)])
-        # rhs extensions
         for rtaint in rhset:
             for id in rtaint.Ddestination_ids:
                 if id in outputset:
-                    print("extension rhs")
-                    print(taint_dict[id].name)
                     greytaints.add(taint_dict[id])
-                    print("Direct Edge " + rtaint.name + " to " + taint_dict[id].name)
                     if id in greydependencies:
                         greydependencies[id].add(GreyDependency("D", rtaint.serial_id))
                     else:
                         greydependencies[id] = set([GreyDependency("D", rtaint.serial_id)])
             for id in rtaint.Idestination_ids:
                 if id in outputset:
-                    print("extension rhs")
-                    print(taint_dict[id].name)
                     greytaints.add(taint_dict[id])
-                    print("Indirect Edge " + rtaint.name + " to " + taint_dict[id].name)
                     if id in greydependencies:
                         greydependencies[id].add(GreyDependency("I", rtaint.serial_id))
                     else:
                         greydependencies[id] = set([GreyDependency("I", rtaint.serial_id)])
             for id in rtaint.Cdestination_ids:
                 if id in outputset:
-                    print("extension rhs")
-                    print(taint_dict[id].name)
                     greytaints.add(taint_dict[id])
-                    print("Control Flow Edge " + rtaint.name + " to " + taint_dict[id].name)
                     if id in greydependencies:
                         greydependencies[id].add(GreyDependency("C", rtaint.serial_id))
                     else:
                         greydependencies[id] = set([GreyDependency("C", rtaint.serial_id)])
         finaltaints = []
-        print("tracked taints")
         for taint in greytaints:
-            print(taint.name)
             if taint.serial_id in greydependencies:
                 finaltaints.append(GreyTaint(taint, list(greydependencies[taint.serial_id])))
             else:
                 finaltaints.append(GreyTaint(taint, []))
         call.taints = finaltaints
-        print(call_trace_dict)
-        print("left")
-        print([t.name for t in lhset])
-        print("right")
-        print([t.name for t in rhset])
-        print("extensions")
-        print([t.name for t in greytaints-lhset-rhset])
         return call
 
+    # transforms trxn [tx_id] from white box to grey box
     def greybox_trxn(self, tx_id):
         tx = self.DFG[tx_id]
         newcalls = []
         for call in tx.call_level_traces:
-            # newcalls.append(self.greybox_call(tx_id, call.call_id))
             if call.call_tree_path == []:
                 rootcall = call
                 break
         for call in tx.call_level_traces:
             if call.call_id != rootcall.call_id:
                 rootcall.taints.extend(call.taints)
-        print("all taints")
-        for taint in rootcall.taints:
-            print(str(taint.serial_id) + " " + taint.name)
         tx.call_level_traces = [rootcall]
         tx.call_level_traces = [self.greybox_call(tx_id, rootcall.call_id)]
 
         return tx
 
 if __name__ == "__main__":
-    trs = Transformer("traces/", 12345679)
-    # call = trs.greybox_call(0, 0)
-    # _pprint(call)
-    # graph = Graph([call])
-    # graph.render("%s_%d.gv" % (12345679, 0))
-    trxn = trs.greybox_trxn(0)
-    _pprint(trxn)
+    trs = Transformer("traces/", 12345958)
+    call = trs.greybox_call(2, 0)
+    graph = Graph([call])
+    graph.render("%s_%d.gv" % (12345958, 0))
+    trxn = trs.greybox_trxn(2)
     graph = Graph(trxn.call_level_traces)
-    graph.render("%s_%d.gv" % (12345679, 111))
+    graph.render("%s_%d.gv" % (12345958, 111))
